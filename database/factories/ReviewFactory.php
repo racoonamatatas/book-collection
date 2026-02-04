491e7d3dbca0603faf55e46d50716e5a8a7150f1
Eoghan Mac An Ghaill <emacanghaill@gmail.com>
2026-02-04 13:00:49 +0100

    Add pivot author_book pivot table

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,8 @@ class ReviewFactory extends Factory
     public function definition(): array
     {
         return [
-            'body' => $this->faker->sentences(),
+            'book_id' => Book::inRandomOrder()->first()->id,
+            'body' => $this->faker->paragraph(),
         ];
     }
 }
--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
 use App\Models\Book;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +15,12 @@ class BookSeeder extends Seeder
     public function run(): void
     {
         //
-        Book::factory()->count(10)->create();
+        Book::factory()->count(50)->create()->each(function ($book)
+        {
+            // Attach 1-3 random authors to each book
+            $book->authors()->attach(
+                Author::inRandomOrder()->limit(rand(1, 3))->pluck('id')
+            );
+        });
     }
 }

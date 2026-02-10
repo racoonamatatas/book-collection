6d76d7075e898d61bb1c9a72e79266c1c3c2a764
racoonamatatas <emacanghaill@gmail.com>
2026-02-10 14:43:00 +0100

    add new router path

--- resources/js/domains/authors/routes.ts
+++ resources/js/domains/authors/routes.ts
@@ -1,8 +1,10 @@
 import { RouteRecordRaw } from "vue-router";
 import OverviewPage from "./pages/OverviewPage.vue";
+import CreatePage from "./pages/CreatePage.vue";
 
 
 
 export const authorRoutes: RouteRecordRaw[] = [
     {path: '/authors', component: OverviewPage, name: 'authors.overview' },
+    {path: '/create', component: CreatePage, name: 'authors.create'}
 ]

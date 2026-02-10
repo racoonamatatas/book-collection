3aef8d58471b2a9bd0e39581bd525b8d6cb3a980
racoonamatatas <emacanghaill@gmail.com>
2026-02-10 15:23:41 +0100

    add router-link to create author

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,6 +3,7 @@
         <router-link :to="{name: 'books.overview'}">Book Overview</router-link>
         <router-link :to="{name: 'books.create'}">Add New Book</router-link>
         <router-link :to="{name: 'authors.overview'}">Author Overview</router-link>
+        <router-link :to="{name: 'authors.create'}">Add New Author</router-link>
 
     </nav>
     <router-view></router-view>    

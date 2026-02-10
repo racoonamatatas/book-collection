1c034b41f355f0c25dddaf76959b9e8c26c2c6b0
racoonamatatas <emacanghaill@gmail.com>
2026-02-10 15:19:34 +0100

    feat: add StoreAuthorRequest

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAuthorRequest;
 use App\Http\Resources\AuthorResource;
 use App\Models\Author;
 use Illuminate\Http\Request;
@@ -14,5 +15,13 @@ class AuthorController extends Controller
     public function index()
     {
         return AuthorResource::collection(Author::all());
+    }
+
+    public function store(StoreAuthorRequest $request)
+    {
+        $author = Author::create($request->validated());
+
+        $authors = Author::all();
+        return AuthorResource::collection($authors);
     }
 }

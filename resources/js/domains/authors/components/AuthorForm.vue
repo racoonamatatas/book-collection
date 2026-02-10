ec3d6747b62cda0537783aee62bd76db8c175276
racoonamatatas <emacanghaill@gmail.com>
2026-02-10 14:26:06 +0100

    Create AuthorForm component

---
+++ resources/js/domains/authors/components/AuthorForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { AuthorData } from '../types/types';
+
+
+const props = defineProps<{author: AuthorData}>();
+
+const emit = defineEmits<{
+    submit: [data: AuthorData]
+}>();
+
+const form = ref<AuthorData>({...props.author});
+
+const handleSubmit = () => emit('submit', form.value);
+</script>
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        <label>Name:</label>
+        <input v-model="form.name" type="text" required />
+
+    </form>
+</template>
--- resources/js/domains/authors/types/types.ts
+++ resources/js/domains/authors/types/types.ts
@@ -1,6 +1,10 @@
-export interface Author {
-  id: number;
+import type { Concrete } from "../../types/types";
+
+export interface AuthorData {
+  id?: number;
   name: string;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
 }
+
+export type Author = Concrete<AuthorData>;
